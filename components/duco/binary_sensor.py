b10525c7ba89d78d50339f53c459008ce1d842bb
Andrew J.Swan <ajs.blackhole@gmail.com>
2026-06-25 17:18:20 +0300

    Update binary_sensor.py

--- components/duco/binary_sensor.py
+++ components/duco/binary_sensor.py
@@ -3,11 +3,13 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import binary_sensor
+from esphome.config_helpers import filter_source_files_from_platform
 from esphome.const import (
     CONF_ID,
     CONF_STATUS,
     DEVICE_CLASS_CONNECTIVITY,
     ENTITY_CATEGORY_DIAGNOSTIC,
+    PlatformFramework,
 )
 
 from . import DucoComponent
@@ -35,3 +37,19 @@ async def to_code(config) -> None:
     if CONF_STATUS in config:
         sens = await binary_sensor.new_binary_sensor(config[CONF_STATUS])
         cg.add(parent.set_status(sens))
+
+
+FILTER_SOURCE_FILES = filter_source_files_from_platform(
+    {
+        "duco_esp32.cpp": {PlatformFramework.ESP8266_ARDUINO},
+        "mining_esp32.cpp": {PlatformFramework.ESP8266_ARDUINO},
+        "duco_esp8266.cpp": {
+            PlatformFramework.ESP32_IDF,
+            PlatformFramework.ESP32_ARDUINO,
+        },
+        "mining_esp8266.cpp": {
+            PlatformFramework.ESP32_IDF,
+            PlatformFramework.ESP32_ARDUINO,
+        },
+    },
+)
