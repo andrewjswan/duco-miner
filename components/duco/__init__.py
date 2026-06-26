cb28353c28ad4ef49f1ccfaa264bef517f64acb8
Andrew J.Swan <ajs.blackhole@gmail.com>
2026-06-26 14:26:20 +0300

    Update __init__.py

--- components/duco/__init__.py
+++ components/duco/__init__.py
@@ -4,7 +4,7 @@ import logging
 
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.const import __version__ as ESPHOME_VERSION
+from esphome.const import __version__ as ESPHOME_VERSION  # noqa: N812
 from esphome import automation
 from esphome.components import ota, sensor, socket
 from esphome.components.esp32 import (
